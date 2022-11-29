3da532ff1c7307d75bd6b879bce22d0879412531
CaolanCode <cmacmahon3@gmail.com>
2022-11-29 14:47:12 +0000

    added digit method

--- say.cpp
+++ say.cpp
@@ -8,12 +8,54 @@
 #include <iostream>
 using namespace std;
 
+string digit(int number){
+    
+    switch (number) {
+        case 1:
+            return "one";
+            break;
+        case 2:
+            return "two";
+            break;
+        case 3:
+            return "three";
+            break;
+        case 4:
+            return "four";
+            break;
+        case 5:
+            return "five";
+            break;
+        case 6:
+            return "six";
+            break;
+        case 7:
+            return "seven";
+            break;
+        case 8:
+            return "eight";
+            break;
+        case 9:
+            return "nine";
+            break;
+            
+        default:
+            return "Error";
+            break;
+    }
+    
+    return "Error";
+}
+
 string getString(long int number){
+    string digitString = "";
+    int digitInt = 0;
     string result = "";
     
-    if(number == 1){
-        result = "one";
-    }
+    digitInt = number % 10;
+    digitString = digit(digitInt);
+    
+    result = result + digitString;
     
     return result;
 }
