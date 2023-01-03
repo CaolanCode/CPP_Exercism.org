d56f79e7c48c2c96ee04e58eab7d0a3f203bb259
CaolanCode <cmacmahon3@gmail.com>
2023-01-03 20:51:09 +0000

    commit basic input output file

--- Book/basicIO.cpp
+++ Book/basicIO.cpp
@@ -1,3 +1,21 @@
-//
-// Created by Caolan Mac Mahon on 03/01/2023.
-//
+/*
+ * Area of rectangle
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length, width;
+
+    cout << "Enter length of rectangle: ";
+    cin >> length;
+
+    cout << "Enter width of rectangle: ";
+    cin >> width;
+
+    cout << "The area is: " << length * width << endl;
+
+    return 0;
+}
