f02463a77eaa2f4d91d1de8d145c1da81c1ccc99
CaolanCode <cmacmahon3@gmail.com>
2023-01-03 21:39:11 +0000

    commit gitignore

---
+++ Book/main.cpp
@@ -0,0 +1,17 @@
+// find area of rectangle
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length, width;
+
+    cout << "Enter the length of the rectangle: ";
+    cin >> length;
+    cout << "Enter the width of the rectangle: ";
+    cin >> width;
+
+    cout << "The area of the rectangle is: " << length * width << endl;
+
+    return 0;
+}
